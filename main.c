c13db7085f6399fdf310e57e2dce9ca35479189d
Thiago T. Moura <rpj134@gmail.com>
2022-06-06 17:42:23 -0300

    organizing code: new functions(have_letter, draw_letters, selec_word)

--- main.c
+++ main.c
@@ -4,21 +4,22 @@
 
 void header();
 void recive_guess();
+int have_letter(char letter);
+void draw_letters();
+void selec_word();
 
+char secret_word[20];
+char tries[26];
+int attempts = 0;
 
 int main()
 {
-    
-
-    char sec_word[20];
-
-    sprintf(sec_word, "MELANCIA"); //Put a word in a char string
+    selec_word();
 
     int win = 0;
     int hanged = 0;
 
-    char tries[26];
-    int attempts = 0;
+
 
     do
     { //begin the game
@@ -26,41 +27,33 @@ int main()
         system("clear");
         header();
 
-        for(int i=0; i<strlen(sec_word); i++)
-        {   
-            int find = 0;
-            for(int j=0; j<attempts; j++)
-            {
-                if(tries[j]==sec_word[i])
-                {
-                    find = 1;
-                    break;
-                }
-            }
-            if(find)
-            {
-                printf("%c ", sec_word[i]);
-            }else
-            {
-                printf("_ ");
-            }
-            
-        }
-        printf("\n");
+        draw_letters();
 
-        recive_guess(tries, &attempts);
+        recive_guess();
 
     }while(!win && !hanged);
 
 }
 
-void recive_guess(char tries[26], int* attempts) //recive the player's guess and save in tries
+int have_letter(char letter) //verify if the user's guess has in the secret word 
+{
+    for(int j=0; j<attempts; j++)
+    {
+        if(tries[j]==letter)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void recive_guess() //recive the player's guess and save in tries
 {
     char guess;
     scanf(" %c", &guess);
 
-    tries[(*attempts)] = guess;
-    (*attempts)++;
+    tries[attempts] = guess;
+    attempts++;
 }
 
 void header() //print game header
@@ -68,4 +61,29 @@ void header() //print game header
     printf("************************\n");
     printf("***   HANGMAN GAME   ***\n");
     printf("************************\n\n");
+}
+
+void draw_letters() //draw the letter if it is in the secret word or draw an underscore if not 
+{
+    for(int i=0; i<strlen(secret_word); i++)
+        {   
+            
+            //here
+            int find = have_letter(secret_word[i]);
+            
+            if(find)
+            {
+                printf("%c ", secret_word[i]);
+            }else
+            {
+                printf("_ ");
+            }
+            
+        }
+        printf("\n");
+}
+
+void selec_word() //select a word and put in a char string
+{
+    sprintf(secret_word, "MELANCIA");
 }
