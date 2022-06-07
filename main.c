3c11003cbbb5b0c0fc5df7c04383a13d6e6b18a4
Thiago T. Moura <rpj134@gmail.com>
2022-06-07 14:03:53 -0300

    making the code stop when user wins or loses

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ void recive_guess();
 int have_letter(char letter);
 void draw_letters();
 void selec_word();
+int hanged();
+int win();
 
 char secret_word[20];
 char tries[26];
@@ -14,12 +16,7 @@ int attempts = 0;
 
 int main()
 {
-    selec_word();
-
-    int win = 0;
-    int hanged = 0;
-
-
+    select_word();
 
     do
     { //begin the game
@@ -31,8 +28,39 @@ int main()
 
         recive_guess();
 
-    }while(!win && !hanged);
+    }while(!win() && !hanged());
 
+}
+
+int win() //know when the user win the game
+{
+    for(int i=0; i<strlen(secret_word); i++)
+    {
+        if(!have_letter(secret_word[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int hanged() //know when the user is hanged
+{
+    int misses = 0;
+    for(int i=0; i<attempts; i++)
+    {
+        int have = 0;
+        for(int j=0; j < strlen(secret_word); j++)
+        {
+            if(tries[i] == secret_word[j])
+            {
+                have = 1;
+                break;
+            }
+        }
+        if(!have) misses++;
+    }
+    return misses >= 5;
 }
 
 int have_letter(char letter) //verify if the user's guess has in the secret word 
@@ -83,7 +111,7 @@ void draw_letters() //draw the letter if it is in the secret word or draw an und
         printf("\n");
 }
 
-void selec_word() //select a word and put in a char string
+void select_word() //select a word and put in a char string
 {
     sprintf(secret_word, "MELANCIA");
 }
